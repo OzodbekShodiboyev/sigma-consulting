39f990d02f72e40e43dff579d3b93df103d170a5
shodiboy_o <ozodbekonline1@gmail.com>
2023-11-02 12:16:52 +0500

    Project design  successfully finished

---
+++ resources/views/index.blade.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <title>Sigma Consulting</title>
+
+
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.png">
+
+
+    <link rel="stylesheet" crossorigin="anonymous" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==">
+    <link href="assets/css/theme.css" rel="stylesheet" />
+
+    <link href="vendors/swiper/swiper-bundle.min.css" rel="stylesheet">
+  </head>
+
+
+  <body>
+
+    <main class="main" id="top">
+      <nav class="navbar navbar-light sticky-top" data-navbar-darken-on-scroll="900">
+        <div class="container pt-2"><a class="navbar-brand" href="index.html"> <img src="logo.png" alt="..." /></a>
+          <div class="navbar-nav ms-auto">
+            <a href="tel:+998996113337" class="btn btn-secondary">+998 99 611 33 37</a>
+          </div>
+        </div>
+      </nav>
+
+
+      <section class="mt-6">
+
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-md-9 col-lg-8 col-xl-5">
+                <img width="100%" src="header.png" alt="" srcset="">
+            </div>
+            <div style="text-align: center" class="col-md-9 col-xl-5">
+                <h2 class="mt-2">XORIJDA GRANT ASOSIDA TALABA BO'LISHNI ISTAYSIZMI?</h2>
+              <p class="fs-2">Sigma Consulting bilan buning imkoni bor!</p>
+              <button class="btn btn-primary mt-3">Talaba bo'lishni istayman</button>
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+
+
+
+      <section>
+
+        <div class="container">
+          <h1 class="display-6 fw-semi-bold" style="text-align: center"> BIZ SIZGA QANDAY YORDAM BERA OLAMIZ:</h1>
+          {{-- <p class="fs-2">Our landing page template works on all devices, so you only have to set.</p> --}}
+          <div class="row mb-4 mt-6">
+            <div class="col-md-6">
+              <div class="border rounded-1 border-700 h-100 features-items">
+                <div class="p-5"><img src="assets/img/gallery/dashboardicon.png" alt="Dashboard" style="width:48px;height:48px;" />
+                  <p class="pt-3 mt-3 mb-0">- Sizga to'g'ri keladigan davlat va universitetni topib, hujjatlaringizni topshiramiz</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="border rounded-1 border-700 h-100 features-items">
+                <div class="p-5"><img src="assets/img/gallery/comment.png" alt="Comment" style="width:48px;height:48px;" />
+                  <p class="mb-0 mt-3 pt-3">- VISA olish jarayonida sizga to'liq yordam beramiz, agar VISA ololmasangiz firma to'lovingiz qaytariladi</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row mb-4">
+            <div class="col-md-6">
+              <div class="border rounded-1 border-700 h-100 features-items">
+                <div class="p-5"><img src="assets/img/gallery/trilored.png" alt="Tailored" style="width:48px;height:48px;" />
+                  <p class="mb-0 mt-3 pt-3">- Bepul ta'lim olishingiz yoki grant asosida o'qishingiz uchun kerakli ko'rsatma va tasviyalarni beramiz</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="border rounded-1 border-700 h-100 features-items">
+                <div class="p-5"><img src="assets/img/gallery/statistics.png" alt="Statistic" style="width:48px;height:48px;" />
+                    <p class="mb-0 mt-3 pt-3">- Mutaxassislarimiz sizga o'qishga joylashguningizga qadar kerak bo'ladigan barcha yordamlarni berishadi</p>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+
+
+
+
+      <section class="mt-5">
+
+        <div class="container">
+          <div class="text-center">
+            <div class="p-5 bg-primary rounded-3">
+              <div class="py-3">
+                <h2 class="mt-3 fs-3 fs-sm-3 latter-sp-3 lh-base fw-semi-bold">Biz  bilan siz dunyoning 100 dan ortiq mamlakatlarida Bakalavr va Magistratura bosqichlarida o'qish imkoniyatiga ega bo'lasiz! </h2>
+              </div>
+              <div class="flex-center d-flex">
+                <button class="btn btn-info">Orzularimga erishmoqchiman <span class="fas fa-arrow-right"></span></button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+
+      <section>
+
+        <div class="container">
+          <h1 class="display-6 fw-semi-bold" style="text-align: center"> Bizni tanlashingiz uchun 5 ta sabab:</h1>
+          {{-- <p class="fs-2">Our landing page template works on all devices, so you only have to set.</p> --}}
+          <div class="row mb-4 mt-6">
+            <div class="col-md-12">
+              <div class="border rounded-1 border-700 h-100 features-items">
+                <div class="p-5"><svg xmlns="http://www.w3.org/2000/svg" height="48px" width="48px" viewBox="0 0 256 512"><style>svg{fill:#7e8bb6}</style><path d="M160 64c0-11.8-6.5-22.6-16.9-28.2s-23-5-32.8 1.6l-96 64C-.5 111.2-4.4 131 5.4 145.8s29.7 18.7 44.4 8.9L96 123.8V416H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h96 96c17.7 0 32-14.3 32-32s-14.3-32-32-32H160V64z"/></svg>
+                  <p class="pt-3 mt-3 mb-0">- Narxlarning hamyonbopligi , agar o'qishga kira olmasangiz yoki VISA ololmasangiz to'lovni amalga oshirmaysiz</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row mb-4">
+            <div class="col-md-12">
+              <div class="border rounded-1 border-700 h-100 features-items">
+                <div class="p-5"><svg xmlns="http://www.w3.org/2000/svg" height="48px" width="48px" viewBox="0 0 320 512"><style>svg{fill:#7e8bb6}</style><path d="M142.9 96c-21.5 0-42.2 8.5-57.4 23.8L54.6 150.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L40.2 74.5C67.5 47.3 104.4 32 142.9 32C223 32 288 97 288 177.1c0 38.5-15.3 75.4-42.5 102.6L109.3 416H288c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9L200.2 234.5c15.2-15.2 23.8-35.9 23.8-57.4c0-44.8-36.3-81.1-81.1-81.1z"/></svg>
+                  <p class="mb-0 mt-3 pt-3">- Ko'p yillik tajribaga ega mutaxassislar</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row mb-4">
+            <div class="col-md-12">
+              <div class="border rounded-1 border-700 h-100 features-items">
+                <div class="p-5"><svg xmlns="http://www.w3.org/2000/svg" height="48px" width="48px" viewBox="0 0 320 512"><style>svg{fill:#7e8bb6}</style><path d="M0 64C0 46.3 14.3 32 32 32H272c13.2 0 25 8.1 29.8 20.4s1.5 26.3-8.2 35.2L162.3 208H184c75.1 0 136 60.9 136 136s-60.9 136-136 136H105.4C63 480 24.2 456 5.3 418.1l-1.9-3.8c-7.9-15.8-1.5-35 14.3-42.9s35-1.5 42.9 14.3l1.9 3.8c8.1 16.3 24.8 26.5 42.9 26.5H184c39.8 0 72-32.2 72-72s-32.2-72-72-72H80c-13.2 0-25-8.1-29.8-20.4s-1.5-26.3 8.2-35.2L189.7 96H32C14.3 96 0 81.7 0 64z"/></svg>
+                  <p class="mb-0 mt-3 pt-3">- IELTS sertifikatisiz ham o'qishga kirish imkoniyatiga egasiz</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row mb-4">
+
+            <div class="col-md-12">
+              <div class="border rounded-1 border-700 h-100 features-items">
+                <div class="p-5"><svg xmlns="http://www.w3.org/2000/svg" height="48px" width="48px" viewBox="0 0 384 512"><style>svg{fill:#7e8bb6}</style><path d="M189 77.6c7.5-16 .7-35.1-15.3-42.6s-35.1-.7-42.6 15.3L3 322.4c-4.7 9.9-3.9 21.5 1.9 30.8S21 368 32 368H256v80c0 17.7 14.3 32 32 32s32-14.3 32-32V368h32c17.7 0 32-14.3 32-32s-14.3-32-32-32H320V160c0-17.7-14.3-32-32-32s-32 14.3-32 32V304H82.4L189 77.6z"/></svg>
+                    <p class="mb-0 mt-3 pt-3">- Orzularingiz ro'yobga chiqishi uchun qo'limizdan kelgan barchasini siz uchun qilamiz</p>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row mb-4">
+            <div class="col-md-12">
+              <div class="border rounded-1 border-700 h-100 features-items">
+                <div class="p-5"><svg xmlns="http://www.w3.org/2000/svg" height="48px" width="48px" viewBox="0 0 320 512"><style>svg{fill:#7e8bb6}</style><path d="M32.5 58.3C35.3 43.1 48.5 32 64 32H256c17.7 0 32 14.3 32 32s-14.3 32-32 32H90.7L70.3 208H184c75.1 0 136 60.9 136 136s-60.9 136-136 136H100.5c-39.4 0-75.4-22.3-93-57.5l-4.1-8.2c-7.9-15.8-1.5-35 14.3-42.9s35-1.5 42.9 14.3l4.1 8.2c6.8 13.6 20.6 22.1 35.8 22.1H184c39.8 0 72-32.2 72-72s-32.2-72-72-72H32c-9.5 0-18.5-4.2-24.6-11.5s-8.6-16.9-6.9-26.2l32-176z"/></svg>
+                    <p class="mb-0 mt-3 pt-3">- Orzularingiz ro'yobga chiqishi uchun qo'limizdan kelgan barchasini siz uchun qilamiz</p>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row mt-6">
+                <button class="btn btn-primary">BEPUL KONSULTATSIYA OLISH <span class="fas fa-arrow-right"></span></button>
+              </div>
+          </div>
+          </div>
+          
+        </div>
+        <!-- end of .container-->
+
+      </section>
+
+
+
+    </main>
+    
+    <script src="vendors/@popperjs/popper.min.js"></script>
+    <script src="vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="vendors/is/is.min.js"></script>
+    <script src="vendors/swiper/swiper-bundle.min.js"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100;200;300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
+    <script src="assets/js/theme.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400&amp;display=swap" rel="stylesheet">
+  </body>
+
+</html>
